c69f8206fea7503bac8a700bf24efacc8bc5d706
Xavi <xavi@geekshubs.com>
2020-07-18 12:39:05 +0200

    Refactor Task Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
+
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    constructor (private taskService: TasksService){}
+
+    @Get()
+    getAllTasks(): Task[]{
+       return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title:string,
+        @Body('description') description:string,
+    ): Task {
+        return this.taskService.createTask(title, description);
+    }
+}
